ff249f400bc08403330fb98f196980ab4e48fb1c
Ivan Vučica <ivan@vucica.net>
2011-01-21 14:06:00 +0100

    Began adding project detail list data source

---
+++ ProjectDetailListDataSource.m
@@ -0,0 +1,30 @@
+/*
+   Project: Zcode
+
+   Copyright (C) 2011 Ivan Vucica
+
+   Author: Ivan Vucica,,,
+
+   Created: 2011-01-21 14:04:00 +0100 by ivucica
+
+   This application is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This application is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU General Public
+   License along with this library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+*/
+
+#import "ProjectDetailListDataSource.h"
+
+
+@implementation ProjectDetailListDataSource
+
+@end
