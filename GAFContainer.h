66256f276820901998cd27749db933da5821ada9
Ivan Vučica <ivan@vucica.net>
2011-01-22 17:12:33 +0100

    Switched license to LGPL2.1 or later

--- GAFContainer.h
+++ GAFContainer.h
@@ -8,18 +8,18 @@
    Created: 2010-12-31 19:00:09 +0100 by ivucica
 
    This application is free software; you can redistribute it and/or
-   modify it under the terms of the GNU General Public
+   modify it under the terms of the GNU Lesser General Public
    License as published by the Free Software Foundation; either
-   version 2 of the License, or (at your option) any later version.
+   version 2.1 of the License, or (at your option) any later version.
 
    This application is distributed in the hope that it will be useful,
    but WITHOUT ANY WARRANTY; without even the implied warranty of
    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-   Library General Public License for more details.
+   Lesser General Public License for more details.
 
-   You should have received a copy of the GNU General Public
-   License along with this library; if not, write to the Free
-   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111 USA.
+   You should have received a copy of the GNU Lesser General Public
+   License along with this application; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 #ifndef _GAFCONTAINER_H_
