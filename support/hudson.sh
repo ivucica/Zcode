f239d597586096b4de6ae8ac961545d0f3526a39
Jason Felice <jason.m.felice@gmail.com>
2011-02-05 16:26:58 +0000

    Fixes for building on 'jerry' CI server

--- support/hudson.sh
+++ support/hudson.sh
@@ -2,6 +2,8 @@
 #
 # CI server build script.
 #
+# Right now this assumes that 'dpkg' is the package manager.
+#
 
 set -ex
 
@@ -25,84 +27,45 @@ find_usable_make() {
 	fi
 }
 
-ensure_we_have_clang() {
-	if ! command -v clang >/dev/null
-	then
-		abort "No 'clang' in path."
-	fi
-}
-
 cache_and_unpack() {
-	local url="$1"
-	local name=$(basename "$url")
-	if ! [[ -f "../$name" ]]
-	then
-		curl "$url" > "../$name"
-	fi
-	tar xzf "../$name"
+       local url="$1"
+       local name=$(basename "$url")
+       if ! [[ -f "../$name" ]]
+       then
+               curl "$url" > "../$name"
+       fi
+       tar xzf "../$name"
 }
 
-#
-# For reproducible builds, we use a gnustep-make specific to this workspace.
-#
-set_up_gnustep() {
-	ensure_we_have_clang
-	export HOME=`dirname $PWD`
-	rm -rf ~/GNUstep
-	mkdir -p ~/GNUstep/GNUstep/Library
+declare -a MAKE_OPTS
+detect_clang() {
+	MAKE_OPTS=()
+	if command -v clang >/dev/null
+	then
+		MAKE_OPTS+=( "CC=clang" "CPP=clang -E" "CXX=clang++" "LD=clang" )
 
-	local GM=gnustep-make-2.4.0
-	cache_and_unpack "ftp://ftp.gnustep.org/pub/gnustep/core/$GM.tar.gz"
-	( cd $GM
-	  CC=clang CPP="clang -E" CXX="clang++" ./configure \
-		--prefix=$HOME/GNUstep/GNUstep \
-		--with-config-file=$HOME/GNUstep/etc/GNUstep.conf \
-		--with-library-combo=gnu-gnu-gnu \
-		--enable-absolute-install-paths
-	  $MAKE 
-	  $MAKE install
-	  )
-	rm -rf $GM
-	. ~/GNUstep/GNUstep/System/Library/Makefiles/GNUstep.sh
+		# Steal gcc's objc runtime
+		OBJC_RUNTIME_PATH=$(dirname $(dirname $(dpkg -S objc/objc.h |awk '{print $2}' |sort |head -n 1)))
+		MAKE_OPTS+=( "OBJCFLAGS+= -I$OBJC_RUNTIME_PATH" )
+	fi
+}
 
-	local GB=gnustep-base-1.21.1
-	cache_and_unpack "ftp://ftp.gnustep.org/pub/gnustep/core/$GB.tar.gz"
-	( cd $GB
-          ./configure
-          $MAKE
-          $MAKE install
-	  )
-	rm -rf $GB
-
-	local GG=gnustep-gui-0.19.0
-	cache_and_unpack "ftp://ftp.gnustep.org/pub/gnustep/core/$GG.tar.gz"
-	( cd $GG
-          ./configure
-          $MAKE
-          $MAKE install
-	  )
-	rm -rf $GG
+invoke_make() {
+	"$MAKE" "${MAKE_OPTS[@]}" "$@"
 }
 
 build_it() {
-	export HOME=`dirname $PWD`
-	. ~/GNUstep/GNUstep/System/Library/Makefiles/GNUstep.sh
+	HOME=$(dirname $PWD); export HOME
+	. /usr/GNUstep/System/Library/Makefiles/GNUstep.sh
+	find_usable_make
+	detect_clang
 
-	$MAKE distclean || true
+	invoke_make distclean || true
 	./configure
-	$MAKE clean
-	$MAKE
-	$MAKE check
+	invoke_make clean
+	invoke_make
+	invoke_make check
 }
 
-find_usable_make
-case "$1" in
---install)	
-	set_up_gnustep
-	;;
---build)
-	build_it
-	;;
-*)
-	abort "Not sure what to do with '$1'"
-esac
+build_it
+exit $?
