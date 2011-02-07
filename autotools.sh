69b8fc28817977a317cc1614970168f2a6d8b206
Jason Felice <jason.m.felice@gmail.com>
2011-02-07 17:59:35 +0000

    autoconf-based configure script

---
+++ autotools.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# Generate files not stored in VCS
+
+aclocal && autoconf && autoheader
--- support/hudson.sh
+++ support/hudson.sh
@@ -2,60 +2,16 @@
 #
 # CI server build script.
 #
-# Right now this assumes that 'dpkg' is the package manager.
-#
 
 set -ex
 
-abort() {
-	printf 'hudson.sh: ' 2>&1
-	printf "$@" 2>&1
-	exit 1
-}
+HOME=$(dirname $PWD); export HOME
+. /usr/GNUstep/System/Library/Makefiles/GNUstep.sh
 
-find_usable_make() {
-	for MAKE in gnumake gmake make :
-	do
-		if command -v $MAKE >/dev/null
-		then
-			break
-		fi
-	done
-	if [[ $MAKE = : ]]
-	then
-		abort "Could not find an acceptable 'make'"
-	fi
-}
+make distclean || true
+./autotools.sh
+./configure
+make distclean
+make
+make check
 
-declare -a MAKE_OPTS
-detect_clang() {
-	MAKE_OPTS=()
-	if command -v clang >/dev/null
-	then
-		MAKE_OPTS+=( "CC=clang" "CPP=clang -E" "CXX=clang++" "LD=clang" )
-
-		# Steal gcc's objc runtime
-		OBJC_RUNTIME_PATH=$(dirname $(dirname $(dpkg -S objc/objc.h |awk '{print $2}' |sort |head -n 1)))
-		MAKE_OPTS+=( "CPPFLAGS+=-I$OBJC_RUNTIME_PATH" )
-	fi
-}
-
-invoke_make() {
-	"$MAKE" "${MAKE_OPTS[@]}" "$@"
-}
-
-build_it() {
-	HOME=$(dirname $PWD); export HOME
-	. /usr/GNUstep/System/Library/Makefiles/GNUstep.sh
-	find_usable_make
-	detect_clang
-
-	invoke_make distclean || true
-	./configure
-	invoke_make clean
-	invoke_make
-	invoke_make check
-}
-
-build_it
-exit $?
