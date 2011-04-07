8c7e46926afa79ceaa771ced47dbb795b2681899
Ivan Vučica <ivan@vucica.net>
2011-04-07 15:05:57 +0200

    Fixed an issue revealed by simple expansion test case. Fixed bug with calling [super setObject:forKey:] when that should not be done. Fixed improper ownership for buildSettings variable. Added -init to get test case to work well.

--- PBXProjLib/PBXBuildSettingsDictionary.m
+++ PBXProjLib/PBXBuildSettingsDictionary.m
@@ -28,13 +28,21 @@
 @implementation PBXBuildSettingsDictionary
 
 #pragma mark nsmutabledictionary methods to pass through
+-(id)init
+{
+    if ((self=[super init]))
+    {
+        buildSettings = [[NSMutableDictionary alloc] init];
+    }
+    return self;
+}
 -(id)initWithDictionary:(NSDictionary *)otherDictionary
 {
     if ((self=[super initWithDictionary:otherDictionary])) 
     {
-        buildSettings = [NSMutableDictionary dictionaryWithDictionary:otherDictionary];
+        buildSettings = [[NSMutableDictionary alloc] initWithDictionary:otherDictionary];
     }
-    return nil;
+    return self;
 }
 -(id)objectForKey:(id)aKey
 {
@@ -42,7 +50,6 @@
 }
 -(void)setObject:(id)anObject forKey:(id)aKey
 {
-    [super setObject:anObject forKey:aKey];
     [buildSettings setObject:anObject forKey:aKey];
 }
 -(void)removeObjectForKey:(id)aKey
@@ -79,7 +86,7 @@
     int currentDictionaryIndex = 0;
     PBXBuildSettingsDictionary *currentDictionary = [expansionDictionaries objectAtIndex:currentDictionaryIndex];
     
-    for (int i = 0; i < [currentString length]; i++) 
+    for (int i = 0; i < [currentString length]-2; i++) 
     {
         NSString *variableOpening = [currentString substringWithRange:NSMakeRange(i, 2)];
         NSRange locationOfVariableCloser = [currentString rangeOfString:@")" options:0 range:NSMakeRange(i+1, [currentString length]-i-1)];
@@ -95,13 +102,12 @@
                 NSString *newString = [currentString substringToIndex:i];
             
                 newString = [newString stringByAppendingString:variableValue];
-                newString = [newString stringByAppendingString:[currentString substringFromIndex:locationOfVariableCloser.location]];
+                newString = [newString stringByAppendingString:[currentString substringFromIndex:locationOfVariableCloser.location+1]];
                 
                 currentString = newString;
                 
                 i--; // repeat the comparison in case we inserted something that is, again, a variable
             }
-            
             // FIXME break evaluation in case variable is already evaluated in
             // this pass over the dictionary!
         }
@@ -129,6 +135,5 @@
     
     return ret;
 }
-
 
 @end
