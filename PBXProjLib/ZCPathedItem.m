6173fae00af0e665b2dad985de308795a5d486c4
Ivan Vučica <ivan@vucica.net>
2011-04-23 01:22:02 +0200

    Fixed case when some pathed item does not have an owner nor an absolute path (for example, a project opened without a ProjectDocument being assigned as an owner, which could happen through commandline tool being used or something similar); this fixes a test case in check. Also, improved code styling.

--- PBXProjLib/ZCPathedItem.m
+++ PBXProjLib/ZCPathedItem.m
@@ -7,21 +7,30 @@
 @synthesize path = path_;
 @synthesize sourceTree = sourceTree_;
 
+// FIXME consider renaming "path" into "fullPath"
+// this should be done to leave "path" as a relative path,
+// and nothing else.
 -(NSString*)path
 {
   // FIXME this also needs to decode certain build-time environment variables
   // sourceTree can contain environment variable name, e.g. "BUILT_PRODUCTS_DIR"
   if([self.sourceTree isEqualToString:@"<absolute>"])
+  {
     return path_;
-  if([self.sourceTree isEqualToString:@"<group>"])
+  }
+  
+  if([self.sourceTree isEqualToString:@"<group>"] && self.owner.path)
   {
     if (path_)
+    {
       return [[self.owner path] stringByAppendingPathComponent:path_];
+    }
     else
+    {
       return self.owner.path;
+    }
   }
-  return self.owner.path;
+  return path_;
 }
-
 
 @end
